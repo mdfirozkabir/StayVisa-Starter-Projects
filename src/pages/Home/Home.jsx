60b36cd0a70680055cc5064f072688658c89cd50
Md. Firoz Kabir <mdfirozkabir.31@gmail.com>
2024-10-28 03:08:40 +0600

    added menu category & completed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
+import Categories from "../../components/Rooms/Categories/Categories"
 import Rooms from "../../components/Rooms/Rooms"
+import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
+
   return (
     <div>
       {/* Categories section */}
+      <Categories />
       {/* Rooms sections */}
-      <Rooms/>
+      <Rooms />
     </div>
   )
 }
