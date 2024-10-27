0b53cda05e853ee9876c4bd992a6db5de42bdca3
Md. Firoz Kabir <mdfirozkabir.31@gmail.com>
2024-10-28 00:45:49 +0600

    setup project StayVista & instal react-spinners,react-router-dom,react-icons,react-hot-toast,react-helmet-async,react-google-charts,react-dom,headlessui/react,stripe/react-stripe-js,tanstack/react-query,axios,date-fns,firebase,query-string,etc

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to StayVista</h1>
+      {/* Categories section */}
+      {/* Rooms sections */}
+    </div>
+  )
+}
+
+export default Home
